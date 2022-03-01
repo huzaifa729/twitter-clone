058094729918c0a009c2e2b28a8ee410dc263a2e
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2022-02-28 21:11:32 -0800

    init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with huzaifa
+          Learn React with huzaifa hello
         </a>
       </header>
     </div>
